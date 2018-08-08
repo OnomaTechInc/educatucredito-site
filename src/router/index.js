225ed1966ad26e78076ad5d5f72fa8833b82101f
julsherms <julius@onomatech.com>
2018-08-09 00:02:41 +0800

    Separate User Dashboard from Front End

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from '@/components/Dashboard'
-import Profile from '@/components/Profile'
-import Files from '@/components/Files'
-import Videos from '@/components/Videos'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
 import PageNotFound from '@/components/404'
@@ -15,20 +11,19 @@ import MoreInfo from '@/components/MoreInfo'
 
 Vue.use(Router)
 
-function requireAuth (to, from, next) {
-  var authenticated = localStorage.getItem('session')
-  if (authenticated === null) {
-    next({
-      path: '/login',
-      query: {
-        redirect: to.fullPath
-      }
-    })
-  } else {
-    next()
-  }
-}
-
+// function requireAuth (to, from, next) {
+//   var authenticated = localStorage.getItem('session')
+//   if (authenticated === null) {
+//     next({
+//       path: '/login',
+//       query: {
+//         redirect: to.fullPath
+//       }
+//     })
+//   } else {
+//     next()
+//   }
+// }
 export default new Router({
   mode: 'hash',
   routes: [{
@@ -102,30 +97,6 @@ export default new Router({
     meta: {
       plainLayout: true
     }
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
-    beforeEnter: requireAuth
-  },
-  {
-    path: '/profile',
-    name: 'Profile',
-    component: Profile,
-    beforeEnter: requireAuth
-  },
-  {
-    path: '/files',
-    name: 'Files',
-    component: Files,
-    beforeEnter: requireAuth
-  },
-  {
-    path: '/videos',
-    name: 'Videos',
-    component: Videos,
-    beforeEnter: requireAuth
   },
   {
     path: '*',
